3fa5876a75556e689c83f9735d27c415e382eb8c
N <34268416+ziloen@users.noreply.github.com>
2025-04-29 00:17:40 +0800

    switch to tsdown

--- tsdown.config.js
+++ tsdown.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'dist',
-  bundle: true,
-  minify: true,
   dts: true,
   format: ['esm'],
   platform: 'node',
