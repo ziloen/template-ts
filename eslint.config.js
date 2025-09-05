eb58eb98d6ee49cb29967983d1699b13a8e3b0ec
N <34268416+ziloen@users.noreply.github.com>
2025-09-05 22:31:00 +0800

    update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import { format, typescript } from '@ziloen/eslint-config'
 
-/** @type { import("@ziloen/eslint-config").FlatESLintConfig[] } */
+/** @type { import("@ziloen/eslint-config").ConfigArray } */
 export default [
   ...typescript({ project: ['./tsconfig.json'] }),
   ...format({ project: ['./tsconfig.json'] }),
